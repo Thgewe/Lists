b919984081d37bd4db7d9f76d979eb991ead2709
Thgewe <hanhatov@mail.ru>
2021-07-23 02:11:39 +0800

    added RecyclerView

--- app/src/main/java/com/example/list/MainActivity.java
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.list;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView itemsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureRecyclerView();
+    }
+
+    private void configureRecyclerView() {
+        itemsView = findViewById(R.id.itemsView);
     }
 }
