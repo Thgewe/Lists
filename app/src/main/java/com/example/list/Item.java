da6a819d341d9873d4d8622f276785bff9d825ea
Thgewe <hanhatov@mail.ru>
2021-07-23 01:39:52 +0800

    added Item

---
+++ app/src/main/java/com/example/list/Item.java
@@ -0,0 +1,19 @@
+package com.example.list;
+
+public class Item {
+    private String title;
+    private String value;
+
+    public Item(String title, String value) {
+        this.title = title;
+        this.value = value;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
