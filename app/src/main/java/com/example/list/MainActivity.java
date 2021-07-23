d5209cf7c7069b2af7360e4cb59bb91c5d82a71a
Thgewe <hanhatov@mail.ru>
2021-07-23 17:54:39 +0800

    first part

--- app/src/main/java/com/example/list/MainActivity.java
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.list;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView itemsView;
+
+    private ItemsAdapter itemsAdapter = new ItemsAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         configureRecyclerView();
+
+        generateItem();
+    }
+
+    private void generateItem() {
+        List<Item> item = new ArrayList<>();
+
+        item.add(new Item("milk", "50"));
+        item.add(new Item("eggs", "100"));
+        item.add(new Item("meat", "500"));
+        item.add(new Item("pan", "2000"));
+        item.add(new Item("milk", "50"));
+        item.add(new Item("eggs", "100"));
+        item.add(new Item("meat", "500"));
+        item.add(new Item("pan", "2000"));
+        item.add(new Item("milk", "50"));
+        item.add(new Item("eggs", "100"));
+        item.add(new Item("meat", "500"));
+        item.add(new Item("pan", "2000"));
+        item.add(new Item("milk", "50"));
+        item.add(new Item("eggs", "100"));
+        item.add(new Item("meat", "500"));
+        item.add(new Item("pan", "2000"));
+        item.add(new Item("milk", "50"));
+        item.add(new Item("eggs", "100"));
+        item.add(new Item("meat", "500"));
+        item.add(new Item("pan", "2000"));
+
+        itemsAdapter.setData(item);
     }
 
     private void configureRecyclerView() {
         itemsView = findViewById(R.id.itemsView);
+        itemsView.setAdapter(itemsAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(),
+                LinearLayoutManager.VERTICAL, false);
+
+        itemsView.setLayoutManager(layoutManager);
     }
 }
